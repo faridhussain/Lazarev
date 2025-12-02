5f5a92da118b40a0c17bf553b07cc5d7ba3c86e7
Farid Hussain <faridhussain0011@gmail.com>
2025-12-02 15:47:09 +0530

    add page5 case studies section with hover video reveal

--- script.js
+++ script.js
@@ -94,4 +94,16 @@ function page3VideoAnimation() {
   });
 }
 
-page3VideoAnimation();
+// page3VideoAnimation();
+
+let sectionRight = document.querySelectorAll(".sec-right");
+sectionRight.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    elem.childNodes[3].style.opacity = 1;
+    elem.childNodes[3].play();
+  });
+  elem.addEventListener("mouseleave", function () {
+    elem.childNodes[3].style.opacity = 0;
+    elem.childNodes[3].load();
+  });
+});
