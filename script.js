d218d67117a47b95d2d1f47608ddf8fd527e1be9
Farid Hussain <faridhussain0011@gmail.com>
2025-12-03 23:21:15 +0530

    locomotive scroll & GSAP ScrollTrigger

--- script.js
+++ script.js
@@ -1,3 +1,65 @@
+function locomotiveAnimation() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
+    smooth: true,
+
+    // for tablet smooth
+    tablet: { smooth: true },
+
+    // for mobile
+    smartphone: { smooth: true },
+  });
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  ScrollTrigger.scrollerProxy(".main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+  });
+
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  ScrollTrigger.refresh();
+}
+locomotiveAnimation();
+
+function loadingAnimation() {
+  var tl = gsap.timeline();
+  tl.from(".page1", {
+    opacity: 0,
+    duration: 0.2,
+    delay: 0.2,
+  });
+  tl.from(".page1", {
+    transform: "scaleX(0.7) scaleY(0.2) translateY(80%)",
+    borderRadius: "150px",
+    duration: 2,
+    ease: "expo.out",
+  });
+  tl.from("nav", {
+    opacity: 0,
+    delay: -0.2,
+  });
+  tl.from(".page1 h1, .page1 p, .page1 div", {
+    opacity: 0,
+    duration: 0.5,
+    stagger: 0.2,
+  });
+}
+loadingAnimation();
+
 function navAnimation() {
   let nav = document.querySelector("nav");
   nav.addEventListener("mouseenter", function () {
@@ -40,7 +102,6 @@ function navAnimation() {
     });
   });
 }
-
 // navAnimation();
 
 function page2Animation() {
@@ -68,7 +129,6 @@ function page2Animation() {
     });
   });
 }
-
 // page2Animation();
 
 function page3VideoAnimation() {
@@ -93,7 +153,6 @@ function page3VideoAnimation() {
     });
   });
 }
-
 page3VideoAnimation();
 
 function page5videoPlayPause() {
@@ -109,7 +168,6 @@ function page5videoPlayPause() {
     });
   });
 }
-
 page5videoPlayPause();
 
 function page8Animation() {
@@ -118,12 +176,11 @@ function page8Animation() {
     duration: 1,
     scrollTrigger: {
       trigger: ".btm8-part2",
-      scroller: "body",
+      scroller: ".main",
       start: "top 80%",
       end: "top 10%",
       scrub: true,
     },
   });
 }
-
 page8Animation();
